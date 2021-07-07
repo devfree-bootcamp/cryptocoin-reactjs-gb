522b7d502918f04514452bbd313cf9a83a12174b
gaboo19 <gabil.tenorio01@gmail.com>
2021-07-06 21:44:10 -0300

    HELLO WORLD

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
+        <h1>HELLO WORLD!</h1>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
