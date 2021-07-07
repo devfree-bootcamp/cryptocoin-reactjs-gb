a769c65bfcc83a3f96e4070f1ac65ec564b5f9c4
gaboo19 <gabil.tenorio01@gmail.com>
2021-07-06 23:24:54 -0300

    change footer

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://pokemoon.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Target {' '}
+          <img src="https://pokemoon.netlify.app/favicon.ico" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
